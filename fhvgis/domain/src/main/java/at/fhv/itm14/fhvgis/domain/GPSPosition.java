484864e50ef6e88eadcd5561a6ebdcc40f669997
marco <sohm.marco@hotmail.de>
2015-10-26 14:50:31 +0100

    GPSPosition, Geofence draft

--- fhvgis/domain/src/main/java/at/fhv/itm14/fhvgis/domain/GPSPosition.java
+++ fhvgis/domain/src/main/java/at/fhv/itm14/fhvgis/domain/GPSPosition.java
@@ -3,5 +3,22 @@ package at.fhv.itm14.fhvgis.domain;
 public class GPSPosition extends Entity {
 
 	// TODO PostGIS or some other API available?!
+	// http://www.java2s.com/Code/Java/2D-Graphics-GUI/Geometry.htm
+	// https://github.com/JavadocMD/simplelatlng
+	private double latitude;
+	private double longitude;
 	
+	public GPSPosition(double latitude, double longitude) {
+		super();
+		this.latitude = latitude;
+		this.longitude = longitude;
+	}
+
+	public double getLatitude() {
+		return latitude;
+	}
+
+	public double getLongitude() {
+		return longitude;
+	}
 }
--- fhvgis/domain/src/main/java/at/fhv/itm14/fhvgis/domain/POI.java
+++ fhvgis/domain/src/main/java/at/fhv/itm14/fhvgis/domain/POI.java
@@ -4,11 +4,20 @@ public class POI extends Entity {
 	
 	private String name;
 	private POIType poiType;
+	private Geofence geofence;
 	
-	public POI(String name, POIType poiType) {
+	public POI(String name, POIType poiType, Geofence geofence) {
 		super();
 		this.name = name;
 		this.poiType = poiType;
+		this.geofence = geofence;
+	}
+	
+	public boolean isWithinGeofence(Waypoint waypoint) {
+		// TODO 
+		// http://www.java2s.com/Code/Java/2D-Graphics-GUI/Geometry.htm
+		// https://github.com/JavadocMD/simplelatlng
+		throw new RuntimeException("Not implemented.");
 	}
 
 	public String getName() {
@@ -19,4 +28,7 @@ public class POI extends Entity {
 		return poiType;
 	}
 
+	public Geofence getGeofence() {
+		return geofence;
+	}
 }
